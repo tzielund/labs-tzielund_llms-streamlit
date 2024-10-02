1fa28e4c10cf410d8b7c93889632e9579d1625f1
TomZielundTR <tom.zielund@thomsonreuters.com>
2024-10-02 11:35:41 -0500

    Added a bit more instruction

--- llms_streamlit.py
+++ llms_streamlit.py
@@ -7,4 +7,6 @@ nltk.download("punkt")
 
 streamlit.write("To start with, find and download some books using the 'book downloader' page")
 streamlit.write("You can view the text of a downloaded book in the 'ebook viewer' page")
-streamlit.write("Finally, create a model using the 'build model from gutengerg file' page and generate some random text with it")
+streamlit.write("Select a passage of text from your book and play the Shannon Game to guess it letter-by-letter")
+streamlit.write("Create a model using the 'build model from gutengerg file' page and generate some random text with it")
+streamlit.write("Use the 'memorization trick' page to memorize a paragraph of text using the power of context.")
